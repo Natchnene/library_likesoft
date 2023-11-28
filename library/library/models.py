10c6c8088fbe7fbc143d21905ab4e620de4f81f9
Natchnene <kondratsiuk.marina@gmail.com>
2023-11-28 20:01:48 +0300

    Create models author, book

---
+++ library/library/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, blank=False)
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return f"{self.pk} - {self.first_name} {self.middle_name} {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250, blank=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    year_publication = models.PositiveSmallIntegerField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return f"{self.isbn} {self.title}"
